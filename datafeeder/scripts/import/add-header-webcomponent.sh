0b5f21a5dcad31f980e44efc8f821491fb697138
Florian Necas <florian.necas@camptocamp.com>
2025-02-06 15:02:16 +0100

    feat: read values from /etc/georchestra/default.properties for gn-ui apps and add config for mapstore

--- datafeeder/scripts/import/add-header-webcomponent.sh
+++ datafeeder/scripts/import/add-header-webcomponent.sh
@@ -2,9 +2,27 @@
 
 # To be properly executed by datafeeder initialization need the +x flag
 # so you need to add it with chmod and commit/push it
+function prop {
+    variable=$(grep "^${1}" /etc/georchestra/default.properties)
+    if  [[ -z $variable ]] || [[ $variable == \#* ]] ;
+    then
+        echo ${2}
+    else
+        echo $variable | cut -d'=' -f2
+    fi
+    #one-line version [[ -z $variable ]] || [[ $variable == \#* ]] && echo ${2} || echo $variable | cut -d'=' -f2
+}
+
+STYLESHEET=$(prop "georchestraStylesheet" "")
+SCRIPT=$(prop "headerScript" "https://cdn.jsdelivr.net/gh/georchestra/header@dist/header.js")
+HEIGHT=$(prop "headerHeight" "80")
+LEGACY=$(prop "useLegacyHeader" "false")
+LEGACY_URL=$(prop "headerUrl" "/header")
+LOGO=$(prop "logoUrl" "https://www.georchestra.org/public/georchestra-logo.svg")
+CONFIG_FILE=$(prop "headerConfigFile" "")
 
 DATAFEEDER_DIR=${1:-/usr/share/nginx/html}
-SNIPPET="<script src='https://cdn.jsdelivr.net/gh/georchestra/header@dist/header.js'></script><geor-header active-app='import' style='height:90px'></geor-header>"
+SNIPPET="<script src='${SCRIPT}'></script><geor-header active-app='datahub' config-file='${CONFIG_FILE}' logo-url='${LOGO}' legacy-header='${LEGACY}' legacy-url='${LEGACY_URL}' height='${HEIGHT}' stylesheet='${STYLESHEET}'></geor-header>"
 
 if grep -q "${SNIPPET}" "${DATAFEEDER_DIR}/index.html"; then
   echo "[INFO] geOrchestra: header already present."
--- datahub/scripts/add-header-webcomponent.sh
+++ datahub/scripts/add-header-webcomponent.sh
@@ -2,9 +2,29 @@
 
 # To be properl executed by datahub initialization need the +x flag 
 # so you need to add it with chmod and commit/push it
+# To be properl executed by datahub initialization need the +x flag
+# so you need to add it with chmod and commit/push it
+function prop {
+    variable=$(grep "^${1}" /etc/georchestra/default.properties)
+    if  [[ -z $variable ]] || [[ $variable == \#* ]] ;
+    then
+        echo ${2}
+    else
+        echo $variable | cut -d'=' -f2
+    fi
+    #one-line version [[ -z $variable ]] || [[ $variable == \#* ]] && echo ${2} || echo $variable | cut -d'=' -f2
+}
+
+STYLESHEET=$(prop "georchestraStylesheet" "")
+SCRIPT=$(prop "headerScript" "https://cdn.jsdelivr.net/gh/georchestra/header@dist/header.js")
+HEIGHT=$(prop "headerHeight" "80")
+LEGACY=$(prop "useLegacyHeader" "false")
+LEGACY_URL=$(prop "headerUrl" "/header")
+LOGO=$(prop "logoUrl" "https://www.georchestra.org/public/georchestra-logo.svg")
+CONFIG_FILE=$(prop "headerConfigFile" "")
 
 DATAHUB_DIR=${1:-/usr/share/nginx/html/datahub}
-SNIPPET="<script src='https://cdn.jsdelivr.net/gh/georchestra/header@dist/header.js'></script><geor-header active-app='datahub' style='height:90px'></geor-header>"
+SNIPPET="<script src='${SCRIPT}'></script><geor-header active-app='datahub' config-file='${CONFIG_FILE}' logo-url='${LOGO}' legacy-header='${LEGACY}' legacy-url='${LEGACY_URL}' height='${HEIGHT}' stylesheet='${STYLESHEET}'></geor-header>"
 
 if grep -q "${SNIPPET}" "${DATAHUB_DIR}/index.html"; then
   echo "[INFO] geOrchestra: header already present."
